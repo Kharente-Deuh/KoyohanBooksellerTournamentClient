be6c0bcc92dcf8a25268714a26bcbcb9152b066a
Baptyste Bonnaudin <bbonnaudin@coyote-group.com>
2022-10-19 17:28:04 +0200

    master: [UPDATE] home page text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,17 @@
 <template>
   <v-container fill-height justify="center">
     <v-row align="center" justify="center">
-      <timer :date="librarianStart" @onFinish="startEvent" />
+      <v-col cols="12">
+        <p class="text">
+          <span class="titleText">Koyohan - The Boxer</span>
+          <br />
+          <span class="gameText">LE JEU</span>
+        </p>
+      </v-col>
+
+      <v-col cols="12">
+        <timer :date="librarianStart" @onFinish="startEvent" />
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -23,3 +33,52 @@ export default class Home extends Vue {
   }
 }
 </script>
+
+<style>
+.leaderboardContainer {
+  padding-left: 0 !important;
+  padding-right: 0 !important;
+  max-height: calc(100% - 70px) !important;
+  height: calc(100% - 70px) !important;
+}
+
+.leaderboardTable {
+  border: 3px solid transparent !important;
+  border-image: linear-gradient(to right, #e11f1c, #74bb85) !important;
+  border-image-slice: 1 !important;
+}
+
+.activeMode {
+  border: solid 1px;
+  border-color: #74bb85;
+}
+
+.membersRow {
+  height: 700px !important;
+  max-height: 700px !important;
+  margin: 0px !important;
+  /* background: white; */
+  border: 1px solid transparent !important;
+  border-image: linear-gradient(to right, #e11f1c, #74bb85) !important;
+  border-image-slice: 1 !important;
+}
+
+#selectionButtonsHeader {
+  margin-top: 8px;
+  margin-bottom: 8px;
+}
+
+.text {
+  font-weight: bold;
+  color: white;
+}
+
+.titleText {
+  font-size: 400%;
+}
+
+.gameText {
+  text-transform: uppercase;
+  font-size: 200%;
+}
+</style>
